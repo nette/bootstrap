b51ea38ea2e63819da5ab6813da1ad57202cc2e5
David Grudl <david@grudl.com>
2014-06-11 19:07:27 +0200

    TracyBridge: fixed collapsePaths for Nette installed by Composer

--- src/Bridges/Framework/TracyBridge.php
+++ src/Bridges/Framework/TracyBridge.php
@@ -23,7 +23,14 @@ class TracyBridge
 	public static function initialize()
 	{
 		$blueScreen = Tracy\Debugger::getBlueScreen();
-		$blueScreen->collapsePaths[] = dirname(dirname(__DIR__));
+		if (preg_match('#(.+)/Bridges/Framework$#', strtr(__DIR__, '\\', '/'), $m)) {
+			if (preg_match('#(.+)/nette/bootstrap/src$#', $m[1], $m2)) {
+				$blueScreen->collapsePaths[] = "$m2[1]/nette";
+				$blueScreen->collapsePaths[] = "$m2[1]/latte";
+			} else {
+				$blueScreen->collapsePaths[] = $m[1];
+			}
+		}
 
 		if (class_exists('Nette\Framework')) {
 			$bar = Tracy\Debugger::getBar();
