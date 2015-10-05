83934fefc66bc7d0348302e21dd4af39edddd3c1
David Grudl <david@grudl.com>
2015-10-05 15:24:40 +0200

    used https

--- src/Bridges/Framework/NetteAccessor.php
+++ src/Bridges/Framework/NetteAccessor.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of the Nette Framework (http://nette.org)
+ * This file is part of the Nette Framework (https://nette.org)
  *
- * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
+ * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
  *
  * For the full copyright and license information, please view
  * the file license.txt that was distributed with this source code.
