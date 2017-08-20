7343bd8cb257991d3c0f46777eea5fc7ec6a10a7
Jan Tvrdík <_github.com@jantvrdik.com>
2017-08-20 19:36:12 +0200

    detectDebugMode() check for Forwarded header existence (#55)

--- src/Bootstrap/Configurator.php
+++ src/Bootstrap/Configurator.php
@@ -308,7 +308,7 @@ class Configurator
 		$list = is_string($list)
 			? preg_split('#[,\s]+#', $list)
 			: (array) $list;
-		if (!isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+		if (!isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !isset($_SERVER['HTTP_FORWARDED'])) {
 			$list[] = '127.0.0.1';
 			$list[] = '::1';
 		}
