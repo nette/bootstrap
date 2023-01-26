ed25974ab83048f6a95a6db29c85fe46bae93a37
David Grudl <david@grudl.com>
2023-01-26 23:44:33 +0100

    event handlers: changed iterable -> array [Closes #71]

--- src/Bootstrap/Configurator.php
+++ src/Bootstrap/Configurator.php
@@ -30,7 +30,7 @@ class Configurator
 
 
 	/** @var callable[]  function (Configurator $sender, DI\Compiler $compiler); Occurs after the compiler is created */
-	public iterable $onCompile = [];
+	public array $onCompile = [];
 
 	public array $defaultExtensions = [
 		'application' => [Nette\Bridges\ApplicationDI\ApplicationExtension::class, ['%debugMode%', ['%appDir%'], '%tempDir%/cache/nette.application']],
